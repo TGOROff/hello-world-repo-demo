4853b1fe3196a3e5ee68bbe5f662be91dc5a9c7f
TGOROff <trumpingcapitalist@gmail.com>
2021-01-23 23:10:48 -0500

    User Input Test No 1

---
+++ userinputtest.py
@@ -0,0 +1,7 @@
+username = input("Enter Username ")
+status = input("Enter server ranking")
+ambitions = input("Enter reason for joining server")
+
+print("/n")
+print("username", "status", "ambitions")
+print(username, status, ambitions)
