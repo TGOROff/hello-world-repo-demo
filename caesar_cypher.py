8bc52d32043f84b0192cd5ec1b6182ca456e3f68
TGOROff <trumpingcapitalist@gmail.com>
2021-01-24 00:22:15 -0500

    Finsihed Caesar Cypher

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -15,11 +15,26 @@ def encrypt(text, s):
             result += chr((ord(char) + s - 97) % 26 + 97)
     return result
 
+def decrypt(result, s):
+    text = ""
+    # transverse the encrypted text
+    for i in range(len(result)):
+        char = result[i]
+        # Decrypt uppercase characters in result
+
+        if (char.isupper()):
+            text += chr((ord(char) - s - 65) % 26 + 65)
+        # Decrypt lowercase characters in result
+        else:
+            text += chr((ord(char) - s - 97) % 26 + 97)
+    return result
+
 
 # check the above function
-text = "CEASER CIPHER DEMO"
-s = 4
+plain_text = input("Enter The Text You Want Encrypted:")
+s = int(input("Enter desired shift:"))
 
-print("Plain Text : " + text)
+print("Plain Text : " + plain_text)
 print("Shift pattern : " + str(s))
-print("Cipher: " + encrypt(text, s))
+print("Cipher: " + encrypt(plain_text, s))
+print("Decipher: " + decrypt(plain_text, s))
